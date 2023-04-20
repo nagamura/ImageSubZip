d865da73050497c0af2c863c5342906c1f325c06
ngamura <nagamura@mitax.co.jp>
2023-04-20 17:15:58 +0900

    modify for v02

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 error_reporting(E_ALL);
+
 // スキャンするディレクトリ名
 $scan_dir = "./data/";
 //zipファイル出力先
 $zip_dir = "./zip/";
 // スキャンするディレクトリ
-$files = scandir($scan_dir);
+$files = glob($scan_dir . "*.jpg");
 // 50MBまで
 $max_filesize = 52428800;
 // 5MBまで(テスト)
@@ -16,34 +17,34 @@ $filesize = 0;
 
 $i = 0;
 $j = 1;
+
 $zip = new ZipArchive();
 $zip_file_name = 'img-';
-$zip_file = $zip_dir . $zip_file_name . $j . ".zip";
-$zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+$img_dir = "img-" . $j;
+mkdir($zip_dir . $img_dir, 0777);
+$zip_file = "img.zip";
+$zip->open($zip_dir . $img_dir . '/' . $zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
 $count = count($files);
 
 foreach ($files as $file) {
-    if ($file != '.' && $file != '..' && $file != '.DS_Store') {
-        $tmp_filesize = filesize($scan_dir . $file);
-        if (($filesize + $tmp_filesize) < $max_filesize) {
-            $filesize += filesize($scan_dir . $file);
-            $zip->addFile($scan_dir . $file, $file);
-            echo $file . " " . $filesize . "\n";
-        } else {
-            echo $file . " " . ($filesize + $tmp_filesize) . "\n";
-            //$filesize = 0;
-            $filesize = $tmp_filesize;
-            $j++;
-            $zip->close();
-            $zip_file = $zip_dir . $zip_file_name . $j . ".zip";
-            $zip = new ZipArchive();
-            $zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
-            $zip->addFile($scan_dir . $file, $file);
-        }        
+    $tmp_filesize = filesize($file);
+    if (($filesize + $tmp_filesize) < $max_filesize) {
+        $filesize += filesize($file);
+        $zip->addFile($file, basename($file));
+        echo $file . " " . $filesize . "\n";
+    } else {
+        echo $file . " " . ($filesize + $tmp_filesize) . "\n";
+        $filesize = $tmp_filesize;
+        $j++;
+        $zip->close();
+        $img_dir = "img-" . $j;
+        mkdir($zip_dir . $img_dir, 0777);
+        $zip = new ZipArchive();
+        $zip->open($zip_dir . $img_dir . '/' .$zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+        $zip->addFile($file, basename($file));
     }
     $i++;
     if ($count == $i) {
         $zip->close();
     }
 }
-?>
